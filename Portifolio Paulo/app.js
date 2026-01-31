c9dfdc23c0d0d98bf50f2147d2bc2accceb20acb
paullomartinsbatista-dot <paullo.martins.batista@gmail.com>
2026-01-30 21:10:38 -0400

    portifolio_html_css_javascript/
    continuação do JS

--- Portifolio Paulo/app.js
+++ Portifolio Paulo/app.js
@@ -1,2 +1,17 @@
-const themeButton = document.getElementById("ThemeToggle")
-    console.log(themeButton)
+const botao = document.getElementById("ThemeToggle")
+    console.log(botao)
+botao.addEventListener('click', function() {
+  console.log("botao clicado");
+    document.body.classList.toggle('dark-mode')
+
+});
+const nameinput = document.getElementById("nameinput")
+console.log("nameinput");
+
+const dispĺayname = document.getElementById("displayname")
+console.log("displayname")
+
+nameinput.addEventListener('input', function(e){
+    dispĺayname.textContent =e.target.value
+});
+
